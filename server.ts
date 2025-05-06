a7031805355fa962dfeca180368c3f3e1a7db489
galiciaabraham <galicia.abraham@hotmail.com>
2025-05-06 08:06:42 -0600

    Started new API application
    - Added package.json with necessary dependencies
    - Created server.ts to set up a basic Express server
    - Configured TypeScript

---
+++ server.ts
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req : any, res: any) => {
+    res.send('Hello World, this will be my new API');
+} )
+
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+})
